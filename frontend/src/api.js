b36c91d41ae4afcb0232de06169708b1eb7a348a
Nimisha Pithala <76213967+NimishaPithala@users.noreply.github.com>
2026-05-07 17:13:09 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://aiui-generator.onrender.com",
-  headers: {
-    "Content-Type": "application/json",
 });
 
 export default API;
