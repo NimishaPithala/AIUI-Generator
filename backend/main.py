01d50d2aa36f49694b52aae8ace0a6abfc4fe84c
Nimisha Pithala <76213967+NimishaPithala@users.noreply.github.com>
2026-05-11 15:35:12 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
 
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 #MODEL = "llama-3.1-8b-instant"
-MoDEL ="llama-3.3-70b-versatile"
+MODEL ="llama-3.3-70b-versatile"
 MAX_REPAIR_ATTEMPTS = 3
 
 
